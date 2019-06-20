4cecd4af989b22d832a3f0981445445f409cbd7b
Joseph Mirth <JoeyMirth@Joeys-MacBook-Pro.local>
2019-06-19 22:27:48 -0500

    not returning anything

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
+form = '''
 <!DOCTYPE html>
 
 <html>
@@ -26,16 +26,17 @@ form = """
         </style>
     </head>
     <body>
-        <form method="POST">
-        <label for="rot">Rotate by:<input type="text name="rot" value="0"></label>    
-        <textarea name="text"></textarea>
-        <input type="submit" value="Submit">
-        
+        <form action='/encrypt' method="POST">
+            <label for="rot">Rotate by:
+            <input type="text" name="rot" value="0"/>    
+            <textarea name="text"></textarea>
+            <input type="submit" value="Submit"/>
+        </form>
     </body>
 </html>
-"""
+'''
 
-@app.route('/', methods=["POST"])
+@app.route('/encrypt', methods=["POST"])
 def encrypt():
     text = request.form["text"]
     rot = int(request.form["rot"])
